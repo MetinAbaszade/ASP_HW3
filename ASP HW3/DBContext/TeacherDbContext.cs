bbf80acc7829749c58150bb39dfecf60e1486a42
Mətin Abaszadə <metin.abaszade@gmail.com>
2022-09-28 11:09:24 +0400

    Add project files.

---
+++ ASP HW3/DBContext/TeacherDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ASP_HW3.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_HW3.DBContext
+{
+    public class TeacherDbContext : DbContext
+    {
+        public TeacherDbContext(DbContextOptions<TeacherDbContext> contextOptions)
+           : base(contextOptions)
+        {
+
+        }
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
---
+++ ASP HW3/Entities/Teacher.cs
@@ -0,0 +1,27 @@
+﻿namespace ASP_HW3.Entities
+{
+    public class Teacher
+    {
+        public Teacher()
+        {
+
+        }
+
+        public Teacher(int ıd, string name, string surname, int age, string address, ushort salary)
+        {
+            Id = ıd;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Address = address;
+            Salary = salary;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public ushort Salary { get; set; }
+    }
+}
